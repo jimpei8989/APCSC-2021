511b2b1279623b66c705e3fb22e0bb5d7e870776
Wu-Jun Pei <jimpei8989@gmail.com>
2021-07-22 14:57:29 +0800

    Remove static cast in pointer_demo

--- Pointers/pointer_demo.cpp
+++ Pointers/pointer_demo.cpp
@@ -4,11 +4,11 @@ int main() {
     int a = 2, b = 3, c = 5;
     int arr[100];
 
-    std::cout << "&a:\t\t" << static_cast<void*>(&a) << std::endl;
-    std::cout << "&b:\t\t" << static_cast<void*>(&b) << std::endl;
-    std::cout << "&c:\t\t" << static_cast<void*>(&c) << std::endl;
+    std::cout << "&a:\t\t" << &a << std::endl;
+    std::cout << "&b:\t\t" << &b << std::endl;
+    std::cout << "&c:\t\t" << &c << std::endl;
 
-    std::cout << "arr:\t\t" << static_cast<void*>(arr) << std::endl;
-    std::cout << "&arr[0]:\t" << static_cast<void*>(&arr[0]) << std::endl;
-    std::cout << "&arr[29]:\t" << static_cast<void*>(&arr[29]) << std::endl;
+    std::cout << "arr:\t\t" << arr << std::endl;
+    std::cout << "&arr[0]:\t" << &arr[0] << std::endl;
+    std::cout << "&arr[29]:\t" << &arr[29] << std::endl;
 }
