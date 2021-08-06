c3b8cdf5f15462e394456f575ddfd80796c0976a
Wu-Jun Pei <jimpei8989@gmail.com>
2021-08-06 10:13:17 +0800

    Fix array01

--- Arrays/array01.cpp
+++ Arrays/array01.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 int main() {
-    int fib[31] = {0, 1};
-    for (int i = 2; i <= 30; i++) {
+    int N;
+    std::cin >> N;
+
+    int fib[100] = {0, 1};
+    for (int i = 2; i <= 99; i++) {
         fib[i] = fib[i - 1] + fib[i - 2];
     }
-    for (int i = 1; i <= 30; i++) {
-        std::cout << fib[i] << (i == 30 ? "\n" : " ");
+    for (int i = 1; i <= N; i++) {
+        std::cout << fib[i] << (i == N ? "\n" : " ");
     }
 }
