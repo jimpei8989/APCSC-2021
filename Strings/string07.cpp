8f087d1961f521c8da89a7205477fb2e391a1f4e
Wu-Jun Pei <jimpei8989@gmail.com>
2021-08-07 10:56:03 +0800

    Fix string07

--- Strings/string07.cpp
+++ Strings/string07.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
+#include <cstring>
 
 int main() {
+    int order[4];
+    for (int i = 0; i < 4; i++) {
+        std::cin >> order[i];
+    }
+
     char a[110], b[110], c[110], d[110];
     std::cin >> a >> b >> c >> d;
 
     char ans[510] = {};
     for (int i = 0; i < 4; i++) {
-        int x;
-        std::cin >> x;
-        if (x == 1) {
+        if (order[i] == 1) {
             strcat(ans, a);
-        } else if (x == 2) {
+        } else if (order[i] == 2) {
             strcat(ans, b);
-        } else if (x == 3) {
+        } else if (order[i] == 3) {
             strcat(ans, c);
-        } else if (x == 4) {
+        } else if (order[i] == 4) {
             strcat(ans, d);
         }
     }
